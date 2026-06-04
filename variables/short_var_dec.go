8a06637a492a35d14e4bf8291d6872511afa2914
nitika2205 <knitika567@gmail.com>
2026-06-04 13:56:35 +0530

    short variable declarations in Go.

---
+++ variables/short_var_dec.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	messageStart := "Happy birthday!You are now" // := is the short form of declaring variables
+	age := 21
+	messageEnd := "years old!"
+	fmt.Println(messageStart, age, messageEnd)
+
+}
+
+/*output:
+Happy birthday!You are now 21 years old!*/
