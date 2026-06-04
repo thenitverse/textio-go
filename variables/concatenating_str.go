91c86eeb95a2e385fa025adf40978d6353404c6e
nitika2205 <knitika567@gmail.com>
2026-06-04 18:21:17 +0530

    concatenating types using + operator.

---
+++ variables/concatenating_str.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "nitika"
+	var password string = "123"
+	fmt.Println("username", username+":  password:", password)
+
+}
+
+// username nitika:  password: 123
