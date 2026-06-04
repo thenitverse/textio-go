06ced0f8305ac0bef872df153772a49dded5c533
nitika2205 <knitika567@gmail.com>
2026-06-04 13:34:55 +0530

    declaring variables in Go.

---
+++ variables/declaring_var.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var smssendinglist int //declaring variable by using var as a keyword and int as a type//
+	smssendinglist = 1000
+	fmt.Printf("your sms sending limit is", smssendinglist)
+}
