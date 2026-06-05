f197d6849cf11388a0579bcf4b7227e6d0b39f60
nitika2205 <knitika567@gmail.com>
2026-06-05 14:04:01 +0530

    Declaring constants in Go.

---
+++ constant_and_formatting/constants.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const premiumPlanName = "Premium Plan"
+	const basicPlanName = "Basic Plan"
+
+	fmt.Println("plan:", premiumPlanName)
+	fmt.Println("plan:", basicPlanName)
+}
+
+/*output:
+plan: Premium Plan
+plan: Basic Plan*/
