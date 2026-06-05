f4d350151b621416fe80ea7083b50ff27c67a232
nitika2205 <knitika567@gmail.com>
2026-06-05 21:10:25 +0530

    using if else conditional for calculating balance.

---
+++ conditionals/calculate_bal.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var insufficientFundMessage string = "Purchase failed. Insufficient funds."
+	var purchaseSuccessMessage string = "Purchase successful."
+	var accountBalance float64 = 100.0
+	var bulkMessageCost float64 = 75.0
+	var isPremiumUser bool = true
+	var discountRate float64 = 0.10
+	var finalCost float64 = bulkMessageCost
+
+	if isPremiumUser {
+		finalCost = bulkMessageCost - (bulkMessageCost * discountRate)
+	}
+	if finalCost <= accountBalance {
+		accountBalance = accountBalance - finalCost
+		fmt.Println(purchaseSuccessMessage)
+
+	} else {
+		fmt.Println(insufficientFundMessage)
+	}
+
+	fmt.Println("Account balance:", accountBalance)
+}
+
+/*output:
+Purchase successful.
+Account balance: 32.5*/
