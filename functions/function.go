391415c8d0c7c33b343c7b8152198dbfa875f023
nitika2205 <knitika567@gmail.com>
2026-06-07 15:43:44 +0530

    functions in Go.

---
+++ functions/function.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func concat(s1 string, s2 string) string {
+	return s1 + s2
+}
+
+func main() {
+	test("Lane,", " happy birthday!")
+	test("Zuck,", " hope that Metaverse thing works out")
+	test("Go", " is fantastic")
+}
+
+func test(s1 string, s2 string) {
+	fmt.Println(concat(s1, s2))
+}
+
+/*output:
+Lane, happy birthday!
+Zuck, hope that Metaverse thing works out
+Go is fantastic*/
